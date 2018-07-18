fa88245b60434ae6dc77c5a32ee22ae32ffda012
Bhupinder Singh <BS029@atcltd.atc.org.nz>
2018-07-18 13:26:58 +1200

    This is my First Project to be uploaded in gIT hup

---
+++ add number/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace add_number
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(textBox1.Text);
+            double num2 = Convert.ToDouble(textBox3.Text);
+            double sum = num1 + num2;
+          //  MessageBox.Show(sum.ToString());
+            textBox2.Text = sum.ToString();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //textBox3.Text = (Convert.ToInt32(textBox1.Text) + Convert.ToInt32(textBox2.Text)).ToString();
+
+        }
+    }
+}
